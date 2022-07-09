28205dbe78605d6be4a886c1b11e633df4f8c47c
Stefan Chen <zchen158@my.bcit.ca>
2022-07-08 21:57:22 -0700

    Display the pokemon id, name, image, type with type color background

--- pokecard/src/App.js
+++ pokecard/src/App.js
@@ -1,38 +1,50 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
+import Thumbnail from "./components/Thumbnail";
 
 function App() {
-
-  const [allPokemons, setAllPokemons] = useState([])
-  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
 
   const getAllPokemons = async () => {
-    const res = await fetch(loadMore)
-    const data = await res.json()
+    const res = await fetch(loadMore);
+    const data = await res.json();
     // 下面这个函数，使你点一下load more就能update loadMore的state, 每次都能load 20个pokemon
-    setLoadMore(data.next)
+    setLoadMore(data.next);
 
     function createPokemonObject(result) {
-      result.forEach( async (pokemon) => {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        const data = await res.json()
+      result.forEach(async (pokemon) => {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        const data = await res.json();
         // 用speard operator ... 让新的data不取代原来的array, 而是Push到原有的array中
-        setAllPokemons(currentList => [...currentList, data])
-      })
+        setAllPokemons((currentList) => [...currentList, data]);
+      });
     }
-    createPokemonObject(data.results)
-
-  }
+    createPokemonObject(data.results);
+    // console.log(allPokemons)
+  };
   // 我希望useEffect里面的东西只run一次，所以第二个参数是一个空的array
   useEffect(() => {
-    getAllPokemons()
-  }, [])
+    getAllPokemons();
+  }, []);
 
   return (
     <div className="app-container">
       <h1>Pokecards</h1>
       <div className="pokemon-container">
         <div className="all-container">
-
+          {allPokemons.map((pokemon, index) => (
+            <Thumbnail
+              id={pokemon.id}
+              name={pokemon.name}
+              image={pokemon.sprites.other.dream_world.front_default}
+              type={pokemon.types[0].type.name}
+              key={index}
+            />
+          ))}
         </div>
         <button className="load-more">Load more</button>
       </div>
